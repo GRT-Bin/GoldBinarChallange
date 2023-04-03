08d1b4493d83a51fd89ab2d94c8ef15b6a7f7d2a
GRT <gratiyowahyu@gmail.com>
2023-04-03 19:34:14 +0700

    Update Routes.jsx

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,7 +3,7 @@ import Home from "./Pages/Home";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import DetailSewa from "./Pages/DetailSewa";
+import DetailSewa from "./Pages/DetailSewaMobil";
 import Formkategori from "./Pages/Formkategori";
 import HasilPencarian from "./Pages/HasilPencarian";
 import "../App.css";
@@ -15,7 +15,7 @@ const Routes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/PencarianMobil" element={<Formkategori />} />
         <Route path="/hasilPencarian" element={<HasilPencarian />} />
-        <Route path="/detailSewa" element={<DetailSewa />} />
+        <Route path="/detailSewaMobil" element={<DetailSewa />} />
       </Routes>
     </BrowserRouter>
   );
